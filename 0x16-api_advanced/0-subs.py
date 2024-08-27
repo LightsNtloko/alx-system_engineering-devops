ce0b05263395689c4535f68960ba03d5b8230d03
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-08-27 03:20:48 +0000

    A function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,12 +17,11 @@ def number_of_subscribers(subreddit):
         int: The number of subscribers for the subreddit. Returns 0 if the
         subreddit is invalid or an error occurs.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         'User-Agent': (
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-            'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 '
-            'Safari/537.36'
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+            '(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         )
     }
 
