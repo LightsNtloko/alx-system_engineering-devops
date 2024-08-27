a364e7d1e1d2a5ff9a3eb143bc4141b86b51f1f2
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-08-27 04:33:17 +0000

    A Write a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,40 +1,21 @@
 #!/usr/bin/python3
-"""Module for querying Reddit API to get the number of subscribers
-for a subreddit."""
-
+"""API user reddit to print out the nubmer of subscribers of a subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Query the Reddit API to get the number of subscribers for a
-    given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit to query.
-
-    Returns:
-        int: The number of subscribers for the subreddit. Returns 0 if the
-        subreddit is invalid or an error occurs.
-    """
+    """Get the number of subscribers in a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        'User-Agent': (
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
-            '(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-        )
-    }
+    headers = {'User.agent': 'request'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
 
-        if response.status_code == 404:
-            return 0
-
-        if response.status_code == 200:
-            data = response.json()
-            return data.get('data', {}).get('subscribers', 0)
-
+    if response.status_code != 200:
         return 0
-    except requests.RequestException:
-        return 0
+
+
+    data = response.json().get("data")
+    num_subs = data.get("subscribers")
+
+
+    return num_subs
